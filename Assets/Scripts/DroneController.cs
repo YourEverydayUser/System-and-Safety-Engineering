c2b7362b7fc7c530c6edccd1e7dd4daf2923c484
Fabian Kuster <f.kuster13@gmail.com>
2024-09-28 11:25:41 +0200

    Fleshed out the scene

--- Assets/Scripts/DroneController.cs
+++ Assets/Scripts/DroneController.cs
@@ -2,12 +2,37 @@ using UnityEngine;
 
 public class DroneController : MonoBehaviour {
     public float speed = 5.0f;
+    public float rotationSpeed = 100.0f;
+
+    private Transform _centerPiece;
+
+    private void Start() {
+        _centerPiece = transform.Find("Center");
+        if (!_centerPiece) {
+            Debug.LogError("Center piece not found in the drone's children objects.");
+        }
+    }
 
     private void Update() {
+        ListenForMovementInput();
+        ListenForRotationInput();
+    }
+
+    private void ListenForMovementInput() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        transform.Translate(movement * speed * Time.deltaTime, Space.Self);
+    }
+
+    private void ListenForRotationInput() {
+        
+        if (Input.GetKey(KeyCode.E)) {
+            transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.Q)) {
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0, Space.World);
+        }
     }
 }
--- Assets/Scripts/PlayerFollow.cs
+++ Assets/Scripts/PlayerFollow.cs
@@ -3,9 +3,16 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour {
     public Transform target;
     public Vector3 offset = new Vector3(0, 2, -5);
+    public float positionSmoothTime = 0.3f;
+    public float rotationSmoothTime = 10f;
 
-    private void LateUpdate() {
-        transform.position = target.position + offset;
-        transform.LookAt(target);
+    private Vector3 _velocity = Vector3.zero;
+
+    void LateUpdate() {
+        Vector3 desiredPosition = target.TransformPoint(offset);
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref _velocity, positionSmoothTime);
+
+        Quaternion desiredRotation = target.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSmoothTime * Time.deltaTime);
     }
 }
